79c676912a5538cfb0aadd9bc8478ddb00528578
Hibiki-2525 <0002.hibiki.2357@gmail.com>
2025-05-27 17:07:49 +0900

    UI変更

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,69 @@
 import { Tabs } from 'expo-router';
+import { Image } from 'react-native';
 
 export default function TabsLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="home" options={{ title: 'Home' }} />
-            <Tabs.Screen name="camera" options={{ title: 'camera' }} />
-            <Tabs.Screen name="memories" options={{ title: 'memories' }} />
-            <Tabs.Screen name="setting" options={{ title: 'setting' }} />
-            <Tabs.Screen name="game" options={{ title: 'game' }} />
+            <Tabs.Screen
+            name="home"
+            options={{
+                title: 'Home',
+                tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                    source={require('../../assets/images/home.png')} // homeアイコンがあれば
+                    style={{ width: size, height: size, tintColor: color }}
+                />
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="memories"
+            options={{
+                title: 'memories',
+                tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                    source={require('../../assets/images/calendar.png')}
+                    style={{ width: size, height: size, tintColor: color }}
+                />
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="camera"
+            options={{
+                title: 'camera',
+                tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                    source={require('../../assets/images/camera.png')}
+                    style={{ width: size, height: size, tintColor: color }}
+                />
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="game"
+            options={{
+                title: 'game',
+                tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                    source={require('../../assets/images/game.png')}
+                    style={{ width: size, height: size, tintColor: color }}
+                />
+                ),
+            }}
+            />
+            <Tabs.Screen
+            name="setting"
+            options={{
+                title: 'setting',
+                tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                    source={require('../../assets/images/settings.png')}
+                    style={{ width: size, height: size, tintColor: color }}
+                />
+                ),
+            }}
+            />
             {/* 他にもタブがあればここに追加 */}
         </Tabs>
     );
