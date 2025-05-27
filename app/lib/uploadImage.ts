67b1535ea6be8f7c11a60e33c5a7f98fd662cf47
Hibiki-2525 <0002.hibiki.2357@gmail.com>
2025-05-27 14:02:54 +0900

    通知機能追加

--- app/lib/uploadImage.ts
+++ app/lib/uploadImage.ts
@@ -1,6 +1,6 @@
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from "@/firebaseConfig";
-import { v4 as uuidv4 } from 'uuid';
+import uuid from 'react-native-uuid';
 import { auth } from "@/firebaseConfig"; // Firebase Auth をインポート
 
 export async function uploadImage(uri: string): Promise<{ imageUrl: string }> {
@@ -12,7 +12,7 @@ export async function uploadImage(uri: string): Promise<{ imageUrl: string }> {
 
         const response = await fetch(uri);
         const blob = await response.blob();
-        const filename = `uploads/${user.uid}/${Date.now()}-${uuidv4()}.jpg`; // ユーザーIDをパスに含める
+        const filename = `uploads/${user.uid}/${Date.now()}-${uuid.v4()}.jpg`; // ユーザーIDをパスに含める
         const storageRef = ref(storage, filename);
 
         await uploadBytes(storageRef, blob);
