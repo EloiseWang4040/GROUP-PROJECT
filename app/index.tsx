1970b9178daf6af436d08ae9b7a33f8893dd89d1
Hibiki-2525 <0002.hibiki.2357@gmail.com>
2025-05-28 12:21:07 +0900

    UI調整

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-// app/(tabs)/index.tsx
+// app/index.tsx
 import { View, Text, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
 import { useFirebaseAuth } from '@/hooks/useFirebaseAuth';
 import { useRouter } from 'expo-router';
 import { showAlert } from '@/utils/alert';
 import { Button, TextInput } from 'react-native-paper';
+import { Stack } from 'expo-router';
 
 export default function LoginScreen() {
     const [email, setEmail] = useState('');
@@ -19,52 +20,55 @@ export default function LoginScreen() {
     }, [user]);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome!</Text>
-            <TextInput
-                mode="outlined"
-                style={styles.input}
-                value={email}
-                onChangeText={setEmail}
-                label="Email"
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <TextInput
-                mode="outlined"
-                style={styles.input}
-                value={password}
-                onChangeText={setPassword}
-                label="Password"
-                secureTextEntry
-            />
-            <Button 
-                mode="contained" 
-                style={styles.button} 
-                contentStyle={styles.buttonContent}
-                onPress={async () => {
-                    const result = await register(email, password);
-                    if (!result.success) {
-                        showAlert('Register エラー', result.message);
-                    }
-                }}
-            >
-                Register
-            </Button>
-            <Button 
-                mode="contained" 
-                style={styles.button} 
-                contentStyle={styles.buttonContent}
-                onPress={async () => {
-                    const result = await signIn(email, password);
-                    if (!result.success) {
-                        showAlert('Sign in エラー', result.message);
-                    }
-                }}
-            >
-                Sign In
-            </Button>
-        </View>
+        <>
+            <Stack.Screen options={{ title: 'login' }} />
+            <View style={styles.container}>
+                <Text style={styles.title}>Welcome!</Text>
+                <TextInput
+                    mode="outlined"
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                    label="Email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    mode="outlined"
+                    style={styles.input}
+                    value={password}
+                    onChangeText={setPassword}
+                    label="Password"
+                    secureTextEntry
+                />
+                <Button 
+                    mode="contained" 
+                    style={styles.button} 
+                    contentStyle={styles.buttonContent}
+                    onPress={async () => {
+                        const result = await register(email, password);
+                        if (!result.success) {
+                            showAlert('Register エラー', result.message);
+                        }
+                    }}
+                >
+                    Register
+                </Button>
+                <Button 
+                    mode="contained" 
+                    style={styles.button} 
+                    contentStyle={styles.buttonContent}
+                    onPress={async () => {
+                        const result = await signIn(email, password);
+                        if (!result.success) {
+                            showAlert('Sign in エラー', result.message);
+                        }
+                    }}
+                >
+                    Sign In
+                </Button>
+            </View>
+        </>
     );
 }
 
